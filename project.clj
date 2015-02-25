9f1973706ffab7889fce84ea6d837d16e3175edc
Leif Walsh <leif.walsh@gmail.com>
2015-02-25 17:07:24 -0500

    use feedparser-clj from clojars

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [compojure "1.3.2"]
-                 [org.clojars.scsibug/feedparser-clj "0.5.0"]]
+                 [adamwynne/feedparser-clj "0.5.2"]]
   :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
   :main ^:skip-aot catgif.core
   :target-path "target/%s"
